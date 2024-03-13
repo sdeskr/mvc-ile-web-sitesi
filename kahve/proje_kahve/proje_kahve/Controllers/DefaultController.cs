978afd38417e4b842442ca3b65927565f288582b
Sude Şakar <118130553+sdeskr@users.noreply.github.com>
2024-03-13 13:07:07 +0300

    mvc basit web sitesi
    mvc kullanarak bir kahve dükkanı web sitesi oluşturma

---
+++ kahve/proje_kahve/proje_kahve/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proje_kahve.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("hakkimizda")]
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        [Route("urunler")]
+        public ActionResult Urunler()
+        {
+            return View();
+        }
+        [Route("magaza")]
+        public ActionResult Magaza()
+        {
+            return View();
+        }
+    }
+}
